744ca006cbc9cbcadce2bbd13379b8dda9f543b0
BasimGunaSekhar <basimgunasekhar@gmail.com>
2026-04-02 19:43:57 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+package com.easy;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n == 0 || n == 1) {
+			System.out.println(n);
+		} else {
+			int a = 0;
+			int b = 1;
+			int c = 0;
+			for (int i = 2; i <= n; i++) {
+				c = a + b;
+				a = b;
+				b = c;
+			}
+			System.out.println(c);
+		}
+	}
+}
